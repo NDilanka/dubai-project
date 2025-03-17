f82ed836d7850f14dd3e25b6d2ba8ac9cfe003ab
Miyuru-Shalitha <miyurushalitha@gmail.com>
2025-03-18 00:12:21 +0530

    sdfs

--- server.ts
+++ server.ts
@@ -44,7 +44,7 @@ const server = Bun.serve({
     "/admin/trade-report": adminPage,
     "/admin/faq-messages": adminPage,
   },
-  development: true,
+  development: false,
   websocket: {
     message: () => { },
   },
